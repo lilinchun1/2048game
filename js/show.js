8c7c0470b7cddd15f24b68be2707821b1de8152f
llc <yxkj@163.com>
2015-02-08 16:35:43 +0800

    add++

--- js/show.js
+++ js/show.js
@@ -1,3 +1,26 @@
 /**
  * Created by Administrator on 2015/2/5.
  */
+function showNumberWihtAnimation(i, j, randNumber) {
+
+    var numberCell = $("#number-cell-"+i+"-"+j);
+    numberCell.css("background-color", getNumberBackgroundColor(randNumber));
+    numberCell.css("color", getNumberColor(randNumber));
+    numberCell.text(randNumber);
+
+    numberCell.animate({
+        width:"100px",
+        height:"100px",
+        top:getPosTop(i, j),
+        left:getPosLeft(i, j)
+    },50);
+}
+
+function showMoveAnimation( fromx, fromy, tox, toy) {
+
+    var numberCell = $("#number-cell-"+fromx+"-"+fromy);
+    numberCell.animate({
+        top:getPosTop(tox,toy),
+        left:getPosLeft(tox,toy)
+    },200);
+}
